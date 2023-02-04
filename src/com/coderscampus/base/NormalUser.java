898628a5189554a37de770014a1f0459da1665c6
James Mitsumasu <jrmitsumasu@gmail.com>
2023-02-04 00:39:52 -0500

    Initial commit
    Inheritance

--- src/com/coderscampus/base/NormalUser.java
+++ src/com/coderscampus/base/NormalUser.java
@@ -1,5 +1,8 @@
 package com.coderscampus.base;
 
 public class NormalUser extends User {
+    public NormalUser() {
+        this.setWriteAccess(Boolean.TRUE);
+    }
 
 }
--- src/com/coderscampus/base/SuperUser.java
+++ src/com/coderscampus/base/SuperUser.java
@@ -1,4 +1,8 @@
 package com.coderscampus.base;
 
 public class SuperUser extends User {
+    public SuperUser() {
+      this.setWriteAccess(Boolean.TRUE);
+      this.setSuperUser(Boolean.TRUE);
+    }
 }
